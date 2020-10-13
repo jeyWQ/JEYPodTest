f06c18065337f617a2f2d5b2e651680acbb31e37
wangquan <wangquan@foorich.com>
2020-10-13 15:15:37 +0800

    Initial KKPodTest’

---
+++ JEYPodTest/Classes/NSDictionary+jlExtension.h
@@ -0,0 +1,13 @@
+//
+//  NSDictionary+extension.h
+//  JLLogEncoding
+//
+//  Created by zhouyu on 16/12/12.
+//  Copyright © 2016年 zhouyu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSDictionary (ZYL)
+
+@end
